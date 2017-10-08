d0b753b273cfbc7461bc592412c90d25a989828d
borsh <young.specialist@gmail.com>
2017-10-08 18:47:40 +0300

    First commit. Added mortage.py

--- mortgage.py
+++ mortgage.py
@@ -9,11 +9,12 @@ class Mortgage():
         self.payment = Mortgage.update_payment(self)
 
     def __str__(self):
-        return "Debt: {} rub,\nMonth rate: {}%,\nPayback period: {} years ({} months,\nPayment: {:.2f} rub".format(self.debt,
-                                                                               self.month_rate*12,
-                                                                               self.payback_period_in_months/12,
-                                                                               self.payback_period_in_months,
-                                                                                                       self.payment)
+        vars_for_format= self.debt, \
+                         self.month_rate*12, \
+                         self.payback_period_in_months/12, \
+                         self.payback_period_in_months, \
+                         self.payment
+        return "Debt: {:.2f} rub,\nMonth rate: {}%,\nPayback period: {:.2f} years ({:d} months,\nPayment: {:.2f} rub".format(*vars_for_format)
 
     def update_payment(self):
         i = self.month_rate
@@ -21,6 +22,31 @@ class Mortgage():
         d = self.debt
         return d*i*((1+i)**n)/((1+i)**n-1)
 
+    def update_debt(self):
+        i = self.month_rate
+        n = self.payback_period_in_months
+        return self.payment/(i*((1+i)**n)/((1+i)**n-1))
+
+    def make_payment(self, payment=None):
+        if payment is None:
+            payment = self.payment
+        self.payback_period_in_months -= 1
+        self.debt = Mortgage.update_debt(self)
+        if payment > self.payment:
+            print("=")
+            self.debt = self.debt - (payment-self.payment)
+            self.payment = Mortgage.update_payment(self)
+        print(Mortgage.__str__(self))
+
 if __name__ == "__main__":
     my = Mortgage(2650000, 7.5, 7)
+    print(my)
+    my.make_payment()
+    print("")
+    print(my)
+    my.make_payment(100000)
+    print("")
+    print(my)
+    my.make_payment()
+    print("")
     print(my)
